4f7a0f9ac553079bb6cdf76a974aee72a362a011
RUGAMBA\AOS <jesus.rugamba@aos.rw>
2023-10-04 11:51:53 +0200

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="" alt="Logo" />
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Reserve</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
