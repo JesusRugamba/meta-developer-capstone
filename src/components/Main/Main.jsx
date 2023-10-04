b5bbc61e58d61c8bdad8f8103c4eab6477b67d05
RUGAMBA\AOS <jesus.rugamba@aos.rw>
2023-10-04 17:07:06 +0200

    little lemon final web app project

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Main.css";
+import Specials from "./Specials/Specials";
+import Testimonials from "./Testimonials/Testimonials";
+import About from "./About/About";
+
+export default function Main() {
+  return (
+    <>
+      <main>
+        <Specials />
+        <Testimonials />
+        <About />
+      </main>
+    </>
+  );
+}
